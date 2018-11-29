75589f894ef16745d747d3edb34a8aa165b5ad90
Roslan Ramli <rroslan@gmail.com>
2018-11-29 22:59:52 +0800

    add features

--- config/app.php
+++ config/app.php
@@ -174,6 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Larapack\Hooks\HooksServiceProvider::class,
+        Larapack\VoyagerHooks\VoyagerHooksServiceProvider::class,
 
     ],
 
